3f05ca63ff27ce893730b0e36410eb77365a7cd6
Nandakishore N M <ceo@particlesystems.in>
2021-08-18 12:30:56 +0530

    Fixed build to host in github pages
    Added base path
    Added all html files to vite config file

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+export default {
+    build: {
+        rollupOptions: {
+          input: {
+            main: path.resolve(__dirname, 'index.html'),
+            about: path.resolve(__dirname, 'about.html'),
+            members: path.resolve(__dirname, 'members.html'),
+            contact: path.resolve(__dirname, 'contact.html'),
+            // nested: resolve(__dirname, 'nested/index.html')
+          }
+        }
+      }
+}
